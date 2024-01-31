32b0d54a8021b2b632f20eedd80067b4e2c0a65b
NitinJuyal1610 <nitinjuyal1610@gmail.com>
2024-01-31 22:20:23 +0530

    json data

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 import express from 'express';
 
-import csvToJson from 'csvtojson';
+import path from 'path';
+import process from 'process';
+
+import csv from 'csvtojson';
+import { sampling } from './utils/sampling_algo.js';
 const app = express();
 
 app.get('/', (req, res) => {
@@ -8,7 +12,15 @@ app.get('/', (req, res) => {
 });
 
 app.get('/getData', async (req, res) => {
+  const csvFilePath = path.join(process.cwd(), 'dataset.csv');
   const jsonArray = await csv().fromFile(csvFilePath);
+
+  // apply sampling algorithm
+
+  const reducedRep = sampling(jsonArray);
+  return res.status(200).json({
+    data: reducedRep,
+  });
 });
 
 app.listen(3000, () => {
